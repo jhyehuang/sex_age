40831a704373ec4348b021f568d2823fab558535
jhyehuang <huangzhijie3918@sina.com>
2018-08-30 07:40:18 +0800

    a

--- arg_data_pre/arg_data_pre.py
+++ arg_data_pre/arg_data_pre.py
@@ -12,7 +12,7 @@ import os
 
 sys.path.append('..')
 
-file_path='../data/sex_age'
+file_path='../../data/sex_age/'
 files=os.listdir(file_path)
 file_names=[]
 for file in files:
@@ -20,5 +20,27 @@ for file in files:
     file_names.append(file_name)
 print(file_names)
 
-data_src=pd.read_csv(file_name,nrows=200,sep='\t')
-print(data_src)
+all_list=[]
+with open(file_path+'deviceid_packages.tsv','r') as fh:
+    for line in fh: 
+        
+        line_list=line.replace('\t',',').replace(' ',',').replace('\n','').split(',')
+        print(line_list)
+        id_dict={}
+        id_no=line_list[0]
+        app_list=line_list[1:]
+        id_dict['id']=id_no
+        for i,x in enumerate(line_list):
+            id_dict['app_'+str(i)]=x
+        
+        all_list.append(id_dict)
+       
+data_src=pd.DataFrame(x for x in all_list)
+#data_src=pd.read_csv(file_path+'package_label.csv')
+
+print(data_src.head(5))
+data_src.to_csv(file_path+'deviceid_packages.csv')
+
+
+
+
