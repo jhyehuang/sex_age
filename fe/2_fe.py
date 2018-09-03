4bfcc71cb126ddde453186ec4eef6b94ea99b46b
zhijie.huang <zhijie.huang@yulore.com>
2018-09-03 12:24:11 +0800

    add fe app_len_t1 app_len_t2

--- fe/2_fe.py
+++ fe/2_fe.py
@@ -33,8 +33,8 @@ def dev_id_train():
 def tx_group_by(tx_pd,col='t1'):
     
     _key_codes = tx_pd[col].values
-    grp1=tx_pd['app_id'].groupby(_key_codes)
-    cnt1 = grp1.aggregate(np.size)
+    cnt1=tx_pd['app_id'].groupby(_key_codes).size()
+#    cnt1 = grp1.aggregate(np.size)
     _cnt = cnt1[_key_codes].values
     _cnt[np.isnan(_cnt)] = 0
     tx_pd[col+'_size'] = _cnt
