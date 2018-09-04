60c440eabec00d3629eff41570b16094ac87d21a
zhijie.huang <zhijie.huang@yulore.com>
2018-09-04 18:01:33 +0800

    add fe app_len_t1 app_len_t2

--- ml/2x11_xgb.py
+++ ml/2x11_xgb.py
@@ -252,8 +252,11 @@ def done(istrain,X_train,y_train,flag):
             dtrain_predprob = xgb1.predict_proba(X_train)
             logging.debug(dtrain_predprob.shape)
             columns=[]
-            for i in dtrain_predprob.shape[1]:
-                columns.append(str(i))
+            for i in range(dtrain_predprob.shape[1]):
+                if flag=='sex':
+                    columns.append(str(i+1))
+                else:
+                    columns.append(str(i))
             y_pred=pd.DataFrame(dtrain_predprob,columns=columns)
             def c(line):
                 return [round(x,6) for x in line]
@@ -273,8 +276,11 @@ def done(istrain,X_train,y_train,flag):
             dtrain_predprob = xgb1.predict_proba(X_train)
             logging.debug(dtrain_predprob.shape)
             columns=[]
-            for i in dtrain_predprob.shape[1]:
-                columns.append(str(i))
+            for i in range(dtrain_predprob.shape[1]):
+                if flag=='sex':
+                    columns.append(str(i+1))
+                else:
+                    columns.append(str(i))
             y_pred=pd.DataFrame(dtrain_predprob,columns=columns)
             def c(line):
                 return [round(x,6) for x in line]
