9f4a71a82e5b3c68c0c9c03d9226aecad4e4a9fe
zhijie.huang <zhijie.huang@yulore.com>
2018-09-05 18:00:09 +0800

    add fe app_len_t1 app_len_t2

--- fe/2_fe.py
+++ fe/2_fe.py
@@ -323,7 +323,9 @@ def devid_app_tfidf(deviceid_packages,package_label):
     deviceid_packages['app_t2_weight']=word_to_tfidf(t2_mtrix)
     lda_pd=word_to_lda(t2_mtrix)
     logging.debug(lda_pd)
-    deviceid_packages=pd.concat([deviceid_packages,lda_pd],axis=1, join_axes=[deviceid_packages.index])
+    for x in ['app_lda_t2_'+str(i) for i in range(1,6)]:
+        deviceid_packages[x]=lda_pd[x]
+#    deviceid_packages=pd.concat([deviceid_packages,lda_pd],axis=1, join_axes=[deviceid_packages.index])
     logging.debug(deviceid_packages)
     
 
