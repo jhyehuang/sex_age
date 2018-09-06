937a4bdf03c01f7fa4b6913ae2cc2ca73a8858b5
zhijie.huang <zhijie.huang@yulore.com>
2018-09-06 11:41:17 +0800

    add fe app_len_t1 app_len_t2

--- ml/model_cv.py
+++ ml/model_cv.py
@@ -335,7 +335,7 @@ def modelfit_multi_cv(alg, X_train, y_train,cv_folds=kfold, early_stopping_round
             alg.set_params(**{key:value})
     #Fit the algorithm on the data
 #    alg.set_params(cvresult.best_params_)
-    alg.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_val, y_val)],eval_metric=['auc'],)
+    alg.fit(X_train, y_train, eval_set=[(X_val, y_val)],eval_metric=['auc'],)
         
     #Predict training set:
     
