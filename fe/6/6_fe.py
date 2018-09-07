0f1a085deafd280d14b6c71f40515ed0840df219
zhijie.huang <zhijie.huang@yulore.com>
2018-09-07 12:28:00 +0800

    add fe app_len_t1 app_len_t2

--- fe/6/6_fe.py
+++ fe/6/6_fe.py
@@ -59,6 +59,8 @@ def word_to_tfidf(word):
         return 0
     elif len(word)==1:
        return [0]
+    elif len(list(set(word)))==1:
+       return [1 for x in range(len(word))]
     transformer=TfidfVectorizer(min_df=1)
     tfidf=transformer.fit_transform(word,)
     weight=np.sum(tfidf.toarray(),axis=1).reshape((-1,1))
