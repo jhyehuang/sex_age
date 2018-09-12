4681a3c19646544a747ca2d1977a5ad9da87126f
zhijie.huang <zhijie.huang@yulore.com>
2018-09-12 18:06:43 +0800

    add fe app_len_t1 app_len_t2

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -128,19 +128,19 @@ def get_train_data(flag='train'):
     deviceid_packages_02 = pd.read_csv(FLAGS.file_path +'02_deviceid_packages.csv',)
     
     
-    deviceid_packages_03 = pd.read_csv(FLAGS.file_path +'03_deviceid_packages.csv',)
-    deviceid_packages_06= pd.read_csv(FLAGS.file_path +'06_deviceid_packages.csv',)
-    deviceid_packages_07= pd.read_csv(FLAGS.file_path +'07_deviceid_packages.csv',)
-    deviceid_packages_08= pd.read_csv(FLAGS.file_path +'08_deviceid_packages.csv',)
-    deviceid_packages_09= pd.read_csv(FLAGS.file_path +'09_deviceid_packages.csv',)
+#    deviceid_packages_03 = pd.read_csv(FLAGS.file_path +'03_deviceid_packages.csv',)
+#    deviceid_packages_06= pd.read_csv(FLAGS.file_path +'06_deviceid_packages.csv',)
+#    deviceid_packages_07= pd.read_csv(FLAGS.file_path +'07_deviceid_packages.csv',)
+#    deviceid_packages_08= pd.read_csv(FLAGS.file_path +'08_deviceid_packages.csv',)
+#    deviceid_packages_09= pd.read_csv(FLAGS.file_path +'09_deviceid_packages.csv',)
 
     deviceid_train=pd.merge(deviceid_train,deviceid_packages_01,on=['device_id'],how='left') 
     deviceid_train=pd.merge(deviceid_train,deviceid_packages_02,on=['device_id'],how='left') 
-    deviceid_train=pd.merge(deviceid_train,deviceid_packages_03,on=['device_id'],how='left') 
-    deviceid_train=pd.merge(deviceid_train,deviceid_packages_06,on=['device_id'],how='left') 
-    deviceid_train=pd.merge(deviceid_train,deviceid_packages_07,on=['device_id'],how='left')
-    deviceid_train=pd.merge(deviceid_train,deviceid_packages_08,on=['device_id'],how='left')
-    deviceid_train=pd.merge(deviceid_train,deviceid_packages_09,on=['device_id'],how='left')
+#    deviceid_train=pd.merge(deviceid_train,deviceid_packages_03,on=['device_id'],how='left') 
+#    deviceid_train=pd.merge(deviceid_train,deviceid_packages_06,on=['device_id'],how='left') 
+#    deviceid_train=pd.merge(deviceid_train,deviceid_packages_07,on=['device_id'],how='left')
+#    deviceid_train=pd.merge(deviceid_train,deviceid_packages_08,on=['device_id'],how='left')
+#    deviceid_train=pd.merge(deviceid_train,deviceid_packages_09,on=['device_id'],how='left')
     
     return deviceid_train
 # flag  in ['all','sex','age','device_id']
