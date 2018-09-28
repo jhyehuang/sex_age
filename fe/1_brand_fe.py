1945af57884679b88e194423fbc7b0b80441aa2d
zhijie.huang <zhijie.huang@yulore.com>
2018-09-28 12:12:47 +0800

    add fe app_len_t1 app_len_t2

--- fe/1_brand_fe.py
+++ fe/1_brand_fe.py
@@ -297,9 +297,9 @@ def type_no_w2(deviceid_packages):
             filte1=np.logical_and(deviceid_train.age==i,deviceid_train.type_no==x)
             filte2=np.logical_and(True,deviceid_train.type_no==x)
             if x in typeno_dict:
-                print(typeno_dict[x])
-                print(deviceid_train.ix[filte1,'type_no'].shape[0])
-                print(deviceid_train.ix[filte2,'type_no'].shape[0])
+                logging.debug(typeno_dict[x])
+                logging.debug(deviceid_train.ix[filte1,'type_no'].shape[0])
+                logging.debug(deviceid_train.ix[filte2,'type_no'].shape[0])
                 typeno_dict[x]=np.sum(typeno_dict[x],(deviceid_train.ix[filte1,'type_no'].shape[0]/deviceid_train.ix[filte2,'type_no'].shape[0]))
 
             else:
