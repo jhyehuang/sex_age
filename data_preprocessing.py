6ee6be17e646f1f3d93247d0757dc48f47058b47
zhijie.huang <zhijie.huang@yulore.com>
2018-10-10 09:42:21 +0800

    add fe app_len_t1 app_len_t2

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -132,17 +132,17 @@ def get_train_data(flag='train'):
     app_len_t2 brand 出现的次数
     
     '''
-    deviceid_packages_02 = pd.read_csv(FLAGS.file_path +'02_deviceid_packages.csv',)
+#    deviceid_packages_02 = pd.read_csv(FLAGS.file_path +'02_deviceid_packages.csv',)
     
     
-    deviceid_packages_03 = pd.read_csv(FLAGS.file_path +'03_deviceid_packages.csv',)
+#    deviceid_packages_03 = pd.read_csv(FLAGS.file_path +'03_deviceid_packages.csv',)
 #    deviceid_packages_07 = pd.read_csv(FLAGS.file_path +'07_deviceid_packages.csv',)
 #    deviceid_packages_09= pd.read_csv(FLAGS.file_path +'09_deviceid_packages.csv',)
 #    deviceid_packages_10= pd.read_csv(FLAGS.file_path +'10_deviceid_packages.csv',)
 
     deviceid_train=pd.merge(deviceid_train,deviceid_packages_01,on=['device_id'],how='left') 
-    deviceid_train=pd.merge(deviceid_train,deviceid_packages_02,on=['device_id'],how='left') 
-    deviceid_train=pd.merge(deviceid_train,deviceid_packages_03,on=['device_id'],how='left')
+#    deviceid_train=pd.merge(deviceid_train,deviceid_packages_02,on=['device_id'],how='left') 
+#    deviceid_train=pd.merge(deviceid_train,deviceid_packages_03,on=['device_id'],how='left')
 #    deviceid_train=pd.merge(deviceid_train,deviceid_packages_07,on=['device_id'],how='left')
 #    deviceid_train=pd.merge(deviceid_train,deviceid_packages_09,on=['device_id'],how='left')
 #    deviceid_train=pd.merge(deviceid_train,deviceid_packages_10,on=['device_id'],how='left')
